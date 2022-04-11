6505d75513ca1e3155e108e007a3531596003527
PavloZubovych <zu10@ukr.net>
2022-04-11 22:46:19 +0300

    fixed bug

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -3,16 +3,25 @@ import PropTypes from 'prop-types';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
   <li class="item">
-    <span class="status">{isOnline}</span>
+    {/* <span class="status">{isOnline}</span> */}
+    {isOnline ? (
+      <span class="status" style={{ backgroundColor: '#55AC57' }}>
+        {isOnline}
+      </span>
+    ) : (
+      <span class="status" style={{ backgroundColor: '##E0343B' }}>
+        {isOnline}
+      </span>
+    )}
     <img class="avatar" src={avatar} alt="User avatar" width="48" />
     <p class="name">{name}</p>
   </li>
 );
 
-FriendListItem.PropTypes = {
-  avatar: PropTypes.string,
-  name: PropTypes.string,
-  isOnline: PropTypes.bool,
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
 
 export default FriendListItem;
