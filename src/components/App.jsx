aa408c8ed848317c50e74d767ad4aa2bd029047b
PavloZubovych <zu10@ukr.net>
2022-04-14 18:27:56 +0300

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import FriendList from "./FriendList/FriendList";
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../user.json';
 import data from '../data.json';
- import friends from '../friends.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json'
 
 
 // console.log(user);
@@ -26,6 +28,7 @@ const App = () => {
       
       <FriendList friends={friends} />
 
+      <TransactionHistory items={transactions} />
 
 
    </div>
