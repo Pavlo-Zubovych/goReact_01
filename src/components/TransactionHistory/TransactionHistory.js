3a9204a5179516c694c3c1fbb24f635cfa169063
PavloZubovych <zu10@ukr.net>
2022-05-04 11:31:56 +0300

    done

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import TransactionItem from '../TransactionItem/TransactionItem';
+import styles from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => (
-  <table className="transaction-history">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
-      </tr>
-    </thead>
+    <table className={styles.transactionHistory}>
+        <thead className={styles.tableHeader}>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
 
-    <tbody>
-      {items.map(item => (
-        <TransactionItem
-          key={item.id}
-          type={item.type}
-          amount={item.amount}
-          currency={item.amount}
-        />
-      ))}
-    </tbody>
-  </table>
+        <tbody>
+            {items.map(item => (
+                <TransactionItem
+                    key={item.id}
+                    type={item.type}
+                    amount={item.amount}
+                    currency={item.currency}
+                />
+            ))}
+        </tbody>
+    </table>
 );
 
 export default TransactionHistory;
--- src/components/TransactionItem/TransactionItem.js
+++ src/components/TransactionItem/TransactionItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styles from './TransactionItem.module.css';
 const TransactionItem = ({ type, amount, currency }) => (
-  <tr className="tabelItem">
-    <td>{type}</td>
-    <td>{amount}</td>
-    <td>{currency}</td>
-  </tr>
+    <tr className={styles.tableItem}>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
+    </tr>
 );
 
 TransactionItem.propTypes = {
-  type: PropTypes.string,
-  amoumt: PropTypes.string,
-  currency: PropTypes.string,
+    type: PropTypes.string,
+    amoumt: PropTypes.string,
+    currency: PropTypes.string,
 };
 
 export default TransactionItem;
