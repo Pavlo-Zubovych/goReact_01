c1b06bde331f97b86d4053ce4d53f585af3a707e
PavloZubovych <zu10@ukr.net>
2022-05-04 11:33:10 +0300

    corected code

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import FriendListItem from '../FriendListItem/FriendListItem';
+import styles from './FriendList.module.css';
 // import PropTypes from 'prop-types';
 
 const FriendList = ({ friends }) => (
-  <ul className="friend-list">
-    {friends.map(friend => (
-      <FriendListItem
-        key={friend.id}
-        avatar={friend.avatar}
-        name={friend.name}
-        isOnline={friend.isOnline}
-      />
-    ))}
-  </ul>
+    <ul className={styles.friendList}>
+        {friends.map(friend => (
+            <FriendListItem
+                key={friend.id}
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+            />
+        ))}
+    </ul>
 );
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
-  <li className="item">
-    {/* <span className="status">{isOnline}</span> */}
-    {isOnline ? (
-      <span className="status" style={{ backgroundColor: '#55AC57' }}>
-        + {isOnline}
-      </span>
-    ) : (
-      <span className="status" style={{ backgroundColor: '#E0343B' }}>
-        - {isOnline}
-      </span>
-    )}
+    <li className={styles.item}>
+        {/* <span className="status">{isOnline}</span> */}
+        {isOnline ? (
+            <span
+                className={styles.status}
+                style={{ backgroundColor: '#55AC57' }}
+            >
+                {isOnline}
+            </span>
+        ) : (
+            <span
+                className={styles.status}
+                style={{ backgroundColor: '#E0343B' }}
+            >
+                {isOnline}
+            </span>
+        )}
 
-    <img className="avatar" src={avatar} alt="User avatar" width="48" />
-    <p className="name">{name}</p>
-  </li>
+        <img
+            className={styles.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+        />
+        <p className={styles.name}>{name}</p>
+    </li>
 );
 
 FriendListItem.propTypes = {
-  avatar: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
 };
 
 FriendListItem.defaultProps = {
-  avatar:
-    'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
+    avatar: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
 };
 
 export default FriendListItem;
