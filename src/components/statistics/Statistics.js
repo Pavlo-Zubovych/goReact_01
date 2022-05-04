058d370a05b6c12c624e1bc4b848457764cd2a9f
PavloZubovych <zu10@ukr.net>
2022-05-04 10:19:32 +0300

    done

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+
+function onRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777216).toString(16);
+}
 
 const Statistics = ({ title, stats }) => (
-  // <div>Hi gays</div>
+    // <div>Hi gays</div>
+    // <section>
+    <section className={styles.statistics}>
+        {title && <h2 className={styles.title}>{title}</h2>}
 
-  <section className="statistics">
-    {title && <h2 className="title">{title}</h2>}
-    <ul className="stat-list"></ul>
-    {stats.map(({ id, label, percentage }) => (
-      <li key={id} className="item">
-        <span className="label">{label}</span>
-        <span className="percentage"> - {percentage}%</span>
-      </li>
-    ))}
-  </section>
+        <ul className={styles.statList}>
+            {stats.map(({ id, label, percentage }) => (
+                <li
+                    key={id}
+                    className={styles.item}
+                    style={{ backgroundColor: onRandomColor() }}
+                >
+                    <span className={styles.label}>{label}</span>
+                    <span className={styles.percentage}>{percentage}%</span>
+                </li>
+            ))}
+        </ul>
+    </section>
 );
 
 Statistics.propTypes = {
-  title: PropTypes.string,
-  id: PropTypes.string,
-  label: PropTypes.string,
-  percentage: PropTypes.number,
+    title: PropTypes.string,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
 };
 
 export default Statistics;
